0199165856d66e03b53d18a45d09a447ade591e6
Konan322447 <chukwuebukae888@gmail.com>
2024-12-20 09:10:40 +0100

    3erf

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const menu = document.querySelector('.mobile-nav');
 const overlay = document.getElementById('filter');
 
 openMenu.addEventListener('click',()=>{
-    menu.style.right = '0';
+    menu.style.zIndex = '2';
     overlay.style.display = 'block';
     document.body.style.overflowY = 'hidden';    
 })
 closeMenu.addEventListener('click',()=>{
-    menu.style.right = '-70vw';
+    menu.style.zIndex = '-4'
     overlay.style.display = 'none';
     document.body.style.overflowY = 'auto';    
 })
